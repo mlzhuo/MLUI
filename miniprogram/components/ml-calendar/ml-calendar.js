35cbe83188e792004ddea1834655d3d33584422a
mlzhuo <lzhuo1995@gmail.com>
2020-07-30 12:43:26 +0800

    优化 ml-calendar 切换月份时选中日期的显示逻辑

--- miniprogram/components/ml-calendar/ml-calendar.js
+++ miniprogram/components/ml-calendar/ml-calendar.js
@@ -2,114 +2,104 @@
 import MLUIUTIL from '../../utils/mlUtil';
 const mlUtils = new MLUIUTIL();
 Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-    yearMonth: {
-      type: String,
-      value: mlUtils.formatDate(new Date()).datestr.slice(0, 7)
-    },
-    dataObj: {
-      type: Array,
-      value: []
-    }
-  },
+	/**
+	 * 组件的属性列表
+	 */
+	properties: {
+		selectedDate: {
+			type: String,
+			value: mlUtils.formatDate(new Date()).datestr
+		},
+		dataObj: {
+			type: Array,
+			value: []
+		}
+	},
 
-  observers: {
-    yearMonth(newVal) {
-      this.setData({ _yearMonth: newVal }, () => {
-        this.setMonthDay();
-      });
-    },
-    dataObj(newVal) {
-      this.setData({ _dataObj: newVal }, () => {
-        this.setMonthDay();
-      });
-    }
-  },
+	observers: {
+		selectedDate(newVal) {
+			this.setData(
+				{
+					_chooseDay: newVal,
+					_today: mlUtils.formatDate(new Date()).datestr
+				},
+				() => {
+					this.setMonthDay();
+				}
+			);
+		},
+		dataObj(newVal) {
+			this.setData({ _dataObj: newVal }, () => {
+				this.setMonthDay();
+			});
+		}
+	},
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
-    _today: '', // 2020-07-26
-    _yearMonth: '',
-    _dataObj: [],
-    _dayList: [],
-    _chooseDay: '' // 2020-07-26
-  },
+	/**
+	 * 组件的初始数据
+	 */
+	data: {
+		_today: '', // 2020-07-26
+		_dataObj: [],
+		_dayList: [],
+		_chooseDay: '' // 2020-07-26
+	},
 
-  lifetimes: {
-    attached() {
-      // 在组件实例进入页面节点树时执行
-      const { datestr } = mlUtils.formatDate(new Date());
-      this.setData(
-        {
-          _today: datestr,
-          _chooseDay: datestr
-        },
-        () => {
-          this.setMonthDay();
-          this._tapDateAction(datestr);
-        }
-      );
-    }
-  },
+	/**
+	 * 组件的方法列表
+	 */
+	methods: {
+		setMonthDay() {
+			const defaultTime = ' 00:00:00';
+			const { selectedDate, _dataObj } = this.data;
+			const { w, datestr, len } = mlUtils.formatDate(
+				selectedDate.slice(0, 7) + '-01' + defaultTime
+			);
+			let _dayList = [];
+			for (let i = 0; i < w; i++) {
+				_dayList.push({});
+			}
 
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-    setMonthDay() {
-      const defaultTime = ' 00:00:00';
-      const { _yearMonth, _dataObj } = this.data;
-      const { w, datestr, len } = mlUtils.formatDate(
-        _yearMonth + '-01' + defaultTime
-      );
-      let _dayList = [];
-      for (let i = 0; i < w; i++) {
-        _dayList.push({});
-      }
-
-      for (let i = 0; i < len; i++) {
-        const _dayItem = mlUtils.formatDate(
-          +new Date(datestr + defaultTime) + i * 24 * 3600 * 1000
-        );
-        _dayList.push({
-          _date: _dayItem.datestr,
-          _day: _dayItem.d,
-          _isShowPoint: _dataObj.findIndex(v => v == _dayItem.datestr) !== -1
-        });
-      }
-      const _last = _dayList.length % 7 === 0 ? 0 : 7 - (_dayList.length % 7);
-      for (let i = 0; i < _last; i++) {
-        _dayList.push({});
-      }
-      let _tempArr = [];
-      for (let i = 0; i < _dayList.length / 7; i++) {
-        _tempArr.push(_dayList.slice(i * 7, i * 7 + 7));
-      }
-      this.setData({
-        _dayList: _tempArr
-      });
-    },
-    _dayTap(e) {
-      const { date } = e.currentTarget.dataset;
-      if (!date) {
-        return;
-      }
-      this.setData(
-        {
-          _chooseDay: date
-        },
-        () => {
-          this._tapDateAction(date);
-        }
-      );
-    },
-    _tapDateAction(date) {
-      this.triggerEvent('tapDateAction', date);
-    }
-  }
+			for (let i = 0; i < len; i++) {
+				const _dayItem = mlUtils.formatDate(
+					+new Date(datestr + defaultTime) + i * 24 * 3600 * 1000
+				);
+				_dayList.push({
+					_date: _dayItem.datestr,
+					_day: _dayItem.d,
+					_isShowPoint:
+						_dataObj.findIndex(v => v == _dayItem.datestr) !== -1
+				});
+			}
+			const _last =
+				_dayList.length % 7 === 0 ? 0 : 7 - (_dayList.length % 7);
+			for (let i = 0; i < _last; i++) {
+				_dayList.push({});
+			}
+			let _tempArr = [];
+			for (let i = 0; i < _dayList.length / 7; i++) {
+				_tempArr.push(_dayList.slice(i * 7, i * 7 + 7));
+			}
+			this.setData({
+				_dayList: _tempArr
+			});
+		},
+		_dayTap(e) {
+			const { date } = e.currentTarget.dataset;
+			if (!date) {
+				return;
+			}
+			this.setData(
+				{
+					_chooseDay: date
+				},
+				() => {
+					this._tapDateAction(date);
+				}
+			);
+		},
+		_tapDateAction(date) {
+			this.triggerEvent('tapDateAction', date);
+		}
+	}
 });
--- miniprogram/pages/mlui/calendar/index.js
+++ miniprogram/pages/mlui/calendar/index.js
@@ -2,61 +2,67 @@
 import MLUIUTIL from '../../../utils/mlUtil';
 const mlutil = new MLUIUTIL();
 Page({
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    date: '',
-    yearMonth: '',
-    dataObj: [
-      '2020-07-03',
-      '2020-07-07',
-      '2020-07-12',
-      '2020-07-13',
-      '2020-07-16',
-      '2020-07-25',
-      '2020-07-26'
-    ]
-  },
+	/**
+	 * 页面的初始数据
+	 */
+	data: {
+		date: '',
+		dataObj: [
+			'2020-07-03',
+			'2020-07-07',
+			'2020-07-12',
+			'2020-07-13',
+			'2020-07-16',
+			'2020-07-25',
+			'2020-07-26'
+		]
+	},
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    const { y, M, datestr } = mlutil.formatDate(new Date());
-    this.setData({
-      date: datestr,
-      yearMonth: y + '-' + M
-    });
-  },
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: function (options) {
+		const { datestr } = mlutil.formatDate(new Date());
+		this.setData({
+			date: datestr
+		});
+	},
 
-  tapDateAction(data) {
-    const date = data.detail;
-    this.setData({ date });
-  },
-  bindDateChange(e) {
-    this.setData({
-      yearMonth: e.detail.value
-    });
-  },
-  preMonth() {
-    const yearMonth = mlutil
-      .formatDate(
-        +new Date(this.data.yearMonth + '-01 00:00:00') - 24 * 3600 * 1000
-      )
-      .datestr.slice(0, 7);
-    this.setData({ yearMonth });
-  },
-  nextMonth() {
-    const { len } = mlutil.formatDate(
-      +new Date(this.data.yearMonth + '-01 00:00:00')
-    );
-    const yearMonth = mlutil
-      .formatDate(
-        +new Date(this.data.yearMonth + '-' + len + ' 00:00:00') +
-          24 * 3600 * 1000
-      )
-      .datestr.slice(0, 7);
-    this.setData({ yearMonth });
-  }
+	tapDateAction(data) {
+		const date = data.detail;
+		this.setData({ date });
+	},
+	bindDateChange(e) {
+		const ym = e.detail.value;
+		let day = this.data.date.slice(8, 10);
+
+		let date = '';
+		const lastDay = new Date(ym.slice(0, 4), ym.slice(5, 7), 0).getDate();
+		if (lastDay >= +day) {
+			date = ym + '-' + day;
+		} else {
+			date = ym + '-' + lastDay;
+		}
+
+		this.setData({ date });
+	},
+	preMonth() {
+		const { date } = this.data;
+		const ym = date.slice(0, 7);
+		const yearMonth = mlutil
+			.formatDate(+new Date(ym + '-01 00:00:00') - 24 * 3600 * 1000)
+			.datestr.slice(0, 7);
+		this.bindDateChange({ detail: { value: yearMonth } });
+	},
+	nextMonth() {
+		const { date } = this.data;
+		const ym = date.slice(0, 7);
+		const { len } = mlutil.formatDate(+new Date(ym + '-01 00:00:00'));
+		const yearMonth = mlutil
+			.formatDate(
+				+new Date(ym + '-' + len + ' 00:00:00') + 24 * 3600 * 1000
+			)
+			.datestr.slice(0, 7);
+		this.bindDateChange({ detail: { value: yearMonth } });
+	}
 });
