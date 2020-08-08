dcf0a6b22c2798f1fad712a5a751788e34d99bf5
mlzhuo <lzhuo1995@gmail.com>
2020-08-08 19:54:12 +0800

    修复滚动数字组件显示问题

--- miniprogram/components/ml-rolling-numbers/ml-rolling-numbers.js
+++ miniprogram/components/ml-rolling-numbers/ml-rolling-numbers.js
@@ -1,54 +1,58 @@
 // components/ml-rolling-numbers/ml-rolling-numbers.js
 let timer;
 Component({
-	/**
-	 * 组件的属性列表
-	 */
-	properties: {
-		number: {
-			type: Number,
-			value: ''
-		},
-		fontSize: {
-			type: Number,
-			value: 14
-		},
-		speed: {
-			type: String,
-			value: 'normal'
-		}
-	},
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    number: {
+      type: Number,
+      value: ''
+    },
+    fontStyle: {
+      type: String,
+      value: ''
+    },
+    speed: {
+      type: String,
+      value: 'default'
+    }
+  },
 
-	/**
-	 * 组件的初始数据
-	 */
-	data: {
-		_numberList: []
-	},
+  observers: {
+    number(newVal) {
+      const _number = newVal.toString();
+      const _numberList = _number.split('');
+      const _tempNumberList = _numberList.map(v => {
+        if (isNaN(+v)) {
+          return v;
+        } else {
+          return 0;
+        }
+      });
+      this.setData({ _numberList: _tempNumberList });
+      timer = setTimeout(v => {
+        this.setData({ _numberList });
+      }, 100);
+    }
+  },
 
-	lifetimes: {
-		attached() {
-			const _number = this.data.number.toString();
-			const _numberList = _number.split('');
-			const _tempNumberList = _numberList.map(v => {
-				if (isNaN(+v)) {
-					return v;
-				} else {
-					return 0;
-				}
-			});
-			this.setData({ _numberList: _tempNumberList });
-			timer = setTimeout(v => {
-				this.setData({ _numberList });
-			}, 100);
-		},
-		detached() {
-			clearTimeout(timer);
-		}
-	},
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    _numberList: []
+  },
 
-	/**
-	 * 组件的方法列表
-	 */
-	methods: {}
+  lifetimes: {
+    attached() {},
+    detached() {
+      clearTimeout(timer);
+    }
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {}
 });
